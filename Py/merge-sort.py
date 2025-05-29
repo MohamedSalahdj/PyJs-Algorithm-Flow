9afd764d8b274dbd6ae87bfaddf409812a211a02
Mohamed Salah <mohamedsamydj@gmail.com>
2025-05-30 02:50:28 +0300

    Add initial structure for merge sort implementation

---
+++ Py/merge-sort.py
@@ -0,0 +1,11 @@
+# Create two function 
+    # 1-  create merge_sort function
+        # 1.1 inputs array, strat, end
+        # 1.2 exist if end >= strat
+        # 1.3 calculate midpoint (strat + end) // 2
+        # 1.4 call merge_sort twice 
+            # 1.4.1 merge_sort(array, start, midpoint)
+            # 1.4.2 merge_sort(array, midpoint + 1, end)
+        # call merge function (array, start, midpoint, end)
+    # 2- create merge function 
+
