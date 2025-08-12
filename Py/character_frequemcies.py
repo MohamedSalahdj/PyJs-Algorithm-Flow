9295ce6efc94f101985968424b0fe616ad73f7a0
Mohamed Salah <mohamedsamydj@gmail.com>
2025-08-12 03:17:16 +0300

    Add `frequency_dict` method to coutn char frequency using dict data structure

--- Py/character_frequemcies.py
+++ Py/character_frequemcies.py
@@ -9,10 +9,23 @@ class CharFrequency:
             if freq[i] > 0:
                 print(f"{chr(i)} --> {freq[i]}")
 
+    def frequency_dict(self, message):
+        freq = {}
+        for i in message:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] = freq[i] + 1
+        
+        for key, value in freq.items():
+            print(f"{key} -- > {value}")
+
 
 def main():
     obj = CharFrequency()
     message = "hello world"
     obj.asci(message)
+    print("-"*45)
+    obj.test(message)
 
 main()
