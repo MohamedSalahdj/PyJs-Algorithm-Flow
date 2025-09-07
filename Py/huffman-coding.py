d4c6ddae29e902fca6676495e8d94a65ac1251f6
Mohamed Salah <mohamedsamydj@gmail.com>
2025-09-07 17:35:19 +0300

    Add `Huffman`with constructor to Calculate frequency, Create a heap node `and add `__repr__`to HeapNode`class

--- Py/huffman-coding.py
+++ Py/huffman-coding.py
@@ -9,3 +9,34 @@ class HeapNode:
     
     def __lt__(self, other):
         return self.freq < other.freq
+
+    def __repr__(self):
+        return f'Node(data: {self.data}, freq: {self.freq})'
+
+
+class Huffman:
+    def __init__(self, message):
+        freqHash = {}
+
+        # Calculate frequency of each character in the message
+        for char in message:
+            if char in freqHash:
+                freqHash[char] += 1
+            else:
+                freqHash[char] = 1
+        
+        # Create a heap node for each character and add it to minHeap
+        self.minHeap = []
+        for char, freq in freqHash.items():
+            newNode = HeapNode(char, freq)
+            heapq.heappush(self.minHeap, newNode)
+
+        print(self.minHeap)
+
+
+
+def main():
+    message = 'internet'
+    huffman = Huffman(message)
+
+main()
